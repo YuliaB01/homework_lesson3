5cd2284fb9884dbe04fb819dcdc2852698171005
Yulia Bilash <starlight05032015@gmail.com>
2019-10-30 18:05:28 +0200

    Minor changes

--- src/api/ApiClient.js
+++ src/api/ApiClient.js
@@ -1,5 +1,5 @@
 export const ApiClient = {
-    apiKey: '=== PASTE YOUR API KEY HERE ===',
+    apiKey: '9b9a6c97be18be80bb60acaad70949ea',
     baseUrl: 'https://api.themoviedb.org/3/tv/',
 
     fetchData: (url) => {
